a2a25df1f4ee54fa0541958ec5deb81c7c9bf0bf
Elaquivala <bombertime4motion@gmail.com>
2023-11-30 10:23:03 +0100

    Add symfony/asset package

---
+++ src/Controller/AcceuilController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AcceuilController extends AbstractController
+{
+    #[Route('/acceuil', name: 'app_acceuil')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/AcceuilController.php',
+        ]);
+    }
+}
